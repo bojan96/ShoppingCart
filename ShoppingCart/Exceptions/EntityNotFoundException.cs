173c65582bf1a6b3b8d3e13e3f9577e63f6661a1
Bojan Malinic <bojan.malinic@outlook.com>
2021-10-26 15:49:12 +0200

    Add EntityNotFoundException

---
+++ ShoppingCart/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ShoppingCart.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(string message)
+            : base(message) { }
+
+        public EntityNotFoundException(int entityId)
+            : this($"Entity with id {entityId} not found") { }
+    }
+}
