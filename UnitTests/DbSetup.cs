29a62c01398c47ae56f2cb4073ecf620fce1d103
Bojan Malinic <bojan.malinic@outlook.com>
2021-10-27 12:11:16 +0200

    Add class where all test data is created

---
+++ UnitTests/DbSetup.cs
@@ -0,0 +1,33 @@
+﻿using ShoppingCart;
+using ShoppingCart.Models.Entity;
+using ShoppingCart.Models.Enums;
+using System;
+
+namespace UnitTests
+{
+    public static class DbSetup
+    {
+        public const int SUBMITTED_CART_ID = 100;
+        public const int DRAFT_CART_ID = 101;
+
+        public static void Seed(ShoppingCartDbContext context)
+        {
+            context.Carts.AddRange(new Cart
+            {
+                Id = SUBMITTED_CART_ID,
+                Status = CartStatus.Submitted,
+                CreatedBy = "user",
+                TimeCreated = DateTime.UtcNow
+            },
+            new Cart
+            {
+                Id = DRAFT_CART_ID,
+                Status = CartStatus.Draft,
+                CreatedBy = "user",
+                TimeCreated = DateTime.UtcNow
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
