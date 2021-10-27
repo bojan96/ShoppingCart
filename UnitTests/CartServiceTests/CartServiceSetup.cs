976a9e4e04faf6ad05efe249f481618c4e421fd1
Bojan Malinic <bojan.malinic@outlook.com>
2021-10-27 12:55:42 +0200

    Move setup code to separate file

---
+++ UnitTests/CartServiceTests/CartServiceSetup.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart;
+using ShoppingCart.AutoMapper;
+using System;
+
+namespace UnitTests.CartServiceTests
+{
+    public partial class CartServiceTests : IDisposable
+    {
+        private readonly IMapper _mapper;
+        private readonly ShoppingCartDbContext _dbContext;
+
+        public CartServiceTests()
+        {
+            _mapper = new MapperConfiguration(config => config.AddProfile<CartMappingProfile>())
+                .CreateMapper();
+            _dbContext = CreateContext();
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        private ShoppingCartDbContext CreateContext()
+        {
+            DbContextOptions<ShoppingCartDbContext> options = new DbContextOptionsBuilder<ShoppingCartDbContext>()
+                .UseInMemoryDatabase("test")
+                .Options;
+
+            ShoppingCartDbContext context = new(options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            // Avoid using seed data from migrations
+            DbSetup.Seed(context);
+            return context;
+        }
+    }
+}
--- UnitTests/CartServiceTests/CartServiceSubmitTests.cs
+++ UnitTests/CartServiceTests/CartServiceSubmitTests.cs
@@ -14,37 +14,8 @@ using Xunit;
 
 namespace UnitTests.CartServiceTests
 {
-    public partial class CartServiceTests : IDisposable
+    public partial class CartServiceTests
     {
-        private readonly IMapper _mapper;
-        private readonly ShoppingCartDbContext _dbContext;
-
-        public CartServiceTests()
-        {
-            _mapper = new MapperConfiguration(config => config.AddProfile<CartMappingProfile>())
-                .CreateMapper();
-            _dbContext = CreateContext();
-        }
-
-        public void Dispose()
-        {
-            _dbContext.Dispose();
-        }
-
-        private ShoppingCartDbContext CreateContext()
-        {
-            DbContextOptions<ShoppingCartDbContext> options = new DbContextOptionsBuilder<ShoppingCartDbContext>()
-                .UseInMemoryDatabase("test")
-                .Options;
-
-            ShoppingCartDbContext context = new(options);
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
-            // Avoid using seed data from migrations
-            DbSetup.Seed(context);
-            return context;
-        }
-
         [Fact]
         public async Task SubmitCartSuccesfully()
         {
