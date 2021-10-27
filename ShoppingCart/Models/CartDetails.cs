d1a919e94f13be6677c8db0af4429f628af4f23f
Bojan Malinic <bojan.malinic@outlook.com>
2021-10-27 12:32:20 +0200

    Make TimeUpdated property nullable

--- ShoppingCart/Models/CartDetails.cs
+++ ShoppingCart/Models/CartDetails.cs
@@ -9,7 +9,7 @@ namespace ShoppingCart.Models
         public int Id { get; set; }
         public CartStatus Status { get; set; }
         public DateTime TimeCreated { get; set; }
-        public DateTime TimeUpdated { get; set; }
+        public DateTime? TimeUpdated { get; set; }
         public string CreatedBy { get; set; }
         public List<CartItemShortDetails> CartItems { get; set; }
     }
